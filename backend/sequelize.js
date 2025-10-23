ee2f5b8d333b664d98147fe2b692a1e173792177
Esteban Eduardo Cardenas Perez <esteban.cardenas@inetum.com>
2025-10-23 09:30:16 -0500

    Actualizacion de conexion a bd

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -1,20 +1,38 @@
 const { Sequelize } = require('sequelize');
 
-const DB_NAME = process.env.DB_NAME || 'kairos';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASS = process.env.DB_PASS || 'CodeEsteban';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 3306;
+let sequelize;
+const DB_URL = process.env.DATABASE_URL || process.env.MYSQL_URL;
+const DB_SSL = String(process.env.DB_SSL || '').toLowerCase() === 'true';
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: 'mysql',
-  logging: false,
-  define: {
-    freezeTableName: true,
-    timestamps: false,
-  },
-});
+if (DB_URL) {
+  // Usar URL completa
+  sequelize = new Sequelize(DB_URL, {
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      freezeTableName: true,
+      timestamps: false,
+    },
+    dialectOptions: DB_SSL ? { ssl: { minVersion: 'TLSv1.2', rejectUnauthorized: true } } : {},
+  });
+} else {
+  // Usar variables separadas
+  const DB_NAME = process.env.DB_NAME || 'kairos';
+  const DB_USER = process.env.DB_USER || 'root';
+  const DB_PASS = process.env.DB_PASS || 'CodeEsteban';
+  const DB_HOST = process.env.DB_HOST || 'localhost';
+  const DB_PORT = process.env.DB_PORT || 3306;
+  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      freezeTableName: true,
+      timestamps: false,
+    },
+    dialectOptions: DB_SSL ? { ssl: { minVersion: 'TLSv1.2', rejectUnauthorized: true } } : {},
+  });
+}
 
 module.exports = sequelize;
